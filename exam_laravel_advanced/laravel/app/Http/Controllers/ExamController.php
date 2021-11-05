72a48021f153cd3d17a73eed9a2141f1e91518a3
msgreentea <msgreentea8@gmail.com>
2021-11-05 17:12:32 +0900

    問題②

--- exam_laravel_advanced/laravel/app/Http/Controllers/ExamController.php
+++ exam_laravel_advanced/laravel/app/Http/Controllers/ExamController.php
@@ -4,21 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB; //DBとしてDBクラスを利用できるようにする
+use App\Models\Exam;
 
 class ExamController extends Controller
 {
     public function index()
     {
-        DB::insert('insert into authors (name, email) values (:name, :email)');
+        DB::insert('insert into users (name, email) values (:name, :email)');
         return view('thanks');
     }
-    public function thanks()
-    // {
+    public function thanks(Request $request)
+    {
         $param = [
             'name' => $request->name,
             'email' => $request->email,
         ];
-    //     return redirect('/');
-    // return view('post');
+        // return redirect('/');
+        return view('thanks');
+        return view('thanks', ('param'));
     }
 }
